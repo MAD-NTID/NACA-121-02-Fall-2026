3e96b9237a991578f0fc7530d17b489422e4a8d5
Michael Berrios <mxbics@rit.edu>
2026-08-28 15:29:05 -0400

    Expressions and Decisions and Switch Demo

---
+++ ExpressionsAndDecisions/Program.cs
@@ -0,0 +1,17 @@
+﻿const double COMMISSION = .07;
+const double MIN_COMMISION_SALE = 10000;
+
+Console.Write("What is sale amount? ");
+double sale_amount = double.Parse(Console.ReadLine());
+
+// Variable for holding Justin's earning
+double commission = 0;
+
+//  Calculate the commissions only if sale is above $10,000
+if (sale_amount >= MIN_COMMISION_SALE)
+{
+    commission = sale_amount * COMMISSION;
+}
+
+// :c is for $ currency
+Console.WriteLine($"Justin's commission for a sale of {sale_amount:c} is {commission:c}");
---
+++ IntrToSwitch/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine($"1 - Choice 1\n2 - Choice 2\n3 - Exit");
+Console.Write("Select a Choice: ");
+
+string choice = Console.ReadLine();
+
+switch (choice)
+{
+    case "1":   
+        //  no break statement between case "1" and "2" so either "1" or "2" will have the same output
+    case "2":
+        Console.WriteLine("Your choice is 1 or 2");
+    break;
+
+    case "3":
+        Console.WriteLine("Your choice is 3 - Exit");
+    break;
+
+    // When the results from the expression has no case match
+    default:
+        Console.WriteLine("Invalid Choice");
+    break;
+}
