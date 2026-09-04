d64cd3c53e00f2907b875cadd8421f87e654d3f7
Michael Berrios <mxbics@rit.edu>
2026-09-04 14:49:13 -0400

    Validation and Exceptions

--- ArrayAndMethods/Program.cs
+++ ArrayAndMethods/Program.cs
@@ -1,1 +1,48 @@
 ﻿Console.WriteLine("Hello, World!");
+
+
+int[] myFirstArray = new int[10];
+
+myFirstArray[0] = 4;
+myFirstArray[1] = 1;
+myFirstArray[2] = 2;
+myFirstArray[3] = 3;
+myFirstArray[9] = 3;
+
+
+System.Console.WriteLine("The length of the array is " + myFirstArray.Length);
+int counter = 0;
+for (counter = 0; counter < 4; counter++)
+{
+    System.Console.WriteLine(myFirstArray[counter]);
+}
+    // System.Console.WriteLine(myFirstArray[1]);
+    // System.Console.WriteLine(myFirstArray[2]);
+    // System.Console.WriteLine(myFirstArray[3]);
+
+int mySum = sum(5,7);
+System.Console.WriteLine("The sum of 5 + 7 is " + mySum);
+mySum = sum(6);
+System.Console.WriteLine("The sum of 6 + is " + mySum);
+
+// int counter = 0;
+// for (counter = 0; counter < 10; counter+=3)
+// {
+//     Console.WriteLine(counter);
+// }
+
+
+// Console.WriteLine("While Loop");
+
+// counter = 0;
+// while (counter < 10)
+// {
+//     Console.WriteLine(counter);
+//     counter +=3;
+// }
+
+ static int sum(int num1, int num2 = 5)
+{
+    
+    return num1 + num2;
+}
---
+++ ValidationException/Program.cs
@@ -0,0 +1,33 @@
+﻿string newData = Console.ReadLine();
+
+// int inputValue = int.Parse( newData );
+// this returns true/false
+// if(int.TryParse(newData, out int inputValue))
+// {
+//     Console.WriteLine($"Successfully converted {inputValue}");
+// }   
+// else
+// {
+//     Console.WriteLine($"Could not convert '{newData}'");
+// } 
+
+// Console.WriteLine(inputValue);
+
+try
+{
+    int inputValue = int.Parse(newData);
+    Console.WriteLine($"Successfully converted {inputValue}");
+
+    Console.WriteLine(inputValue);
+}
+// We can catch specific exception
+// catch (FormatException fe)
+// {
+//     Console.WriteLine($"Could not convert '{newData}'");
+//     Console.WriteLine($"{fe.Message}");
+// } 
+catch (Exception e)
+{
+    Console.WriteLine($"Could not convert '{newData}'");
+    Console.WriteLine($"{e.Message}");
+} 
